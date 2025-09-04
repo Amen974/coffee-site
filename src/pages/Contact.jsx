f0b9a54b71b018ef17674e1addd57068ab5a778e
amen <aamenallah593@gmail.com>
2025-09-04 13:15:02 +0100

    make the contact responsive

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,34 +2,37 @@ import { Instagram, Facebook, Twitter, Mail } from "lucide-react";
 
 const Contact = () => {
   const InputStyle =
-    "border-red-700 border-solid border-2 rounded-md w-[200px] h-10 p-2 bg-black focus:outline-none";
+    "border-red-700 border-solid border-2 rounded-md w-[200px] h-10 p-2 focus:outline-none max-sm:w-full";
   const TextAreaStyle =
-    "border-red-700 border-solid border-2 rounded-md w-full h-[120px] p-2 bg-black resize-none focus:outline-none";
+    "border-red-700 border-solid border-2 rounded-md w-full h-[120px] p-2 resize-none focus:outline-none";
 
   return (
-    <main className="Contact h-screen w-full bg-black flex justify-center items-center text-white">
-      <div className=" flex justify-center p-10 gap-10 max-w-4xl w-full">
+    <main className="Contact h-screen w-full bg-black flex justify-center items-center text-white max-sm:h-[100%]">
+      <div className=" flex justify-center p-10 gap-10 max-w-4xl w-full max-sm:flex-col">
         
         {/* Left side */}
-        <div className="flex flex-col justify-center gap-8 w-1/2">
+        <div className="flex flex-col justify-center gap-8 w-1/2 max-sm:w-full">
           <div>
-            <h1 className="text-red-700 font-bold font-dancing text-6xl mb-3">
+            <h1 className="text-red-700 font-bold font-dancing text-6xl mb-3 max-sm:text-center">
               Get in Touch
             </h1>
-            <p className="text-[18px] text-gray-300">
+            <p className="text-[18px] text-gray-300 max-sm:text-center">
               We&apos;d love to hear from you.
             </p>
           </div>
-          <p className="max-w-[300px] text-gray-400">
+          <div className="flex max-md:justify-center">
+            <p className="max-w-[300px] text-gray-400 max-sm:text-center">
             Whether you have questions, feedback, or just want to say hello,
             feel free to reach out.
-          </p>
+            </p>
+          </div>
+          
           <div className="flex flex-col gap-3">
-            <div className="flex items-center">
-              <Mail size={18} />
+            <div className="flex items-center max-sm:justify-center">
+              <Mail size={18} className="max-sm:hidden" />
               <span className="text-sm ml-2">hello@coffeehouse.com</span>
             </div>
-            <nav className="flex gap-4 mt-2">
+            <nav className="flex gap-4 mt-2 max-sm:hidden">
               <a href="#" className="hover:text-red-700">
                 <Instagram size={24} />
               </a>
@@ -44,9 +47,9 @@ const Contact = () => {
         </div>
 
         {/* Right side */}
-        <div className="w-1/2">
+        <div className="w-1/2 max-sm:w-full">
           <form className="flex flex-col gap-4">
-            <div className="flex flex-row gap-4">
+            <div className="flex flex-row gap-4 max-md:flex-col">
               <div className="flex flex-col">
                 <label htmlFor="FirstName" className="text-sm mb-1">
                   First Name
@@ -77,7 +80,7 @@ const Contact = () => {
 
             <button
               type="submit"
-              className="bg-red-700 text-[1rem] px-6 py-2 rounded-md shadow-md hover:bg-red-800 transition duration-200 w-fit"
+              className="bg-red-700 text-[1rem] px-6 py-2 rounded-md shadow-md hover:bg-red-800 transition duration-200 w-fit max-sm:w-full"
             >
               Send
             </button>
