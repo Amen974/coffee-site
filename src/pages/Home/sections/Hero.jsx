4f1d0f736d2ecd9cca2f13ffa3bbc7320d0fa9dd
amen <aamenallah593@gmail.com>
2025-11-03 11:49:15 +0100

    add animation to hero section + the first div in the best section

--- src/pages/Home/sections/Hero.jsx
+++ src/pages/Home/sections/Hero.jsx
@@ -1,6 +1,40 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import { HashLink } from "react-router-hash-link";
 
 const Hero = () => {
+  useGSAP(()=>{
+    gsap.to('.hero-h1',{
+      opacity:1,
+      y:0,
+      ease:'power1.inOut',
+    })
+    gsap.fromTo('.hero-p',{
+      opacity:0,
+      y:20
+    },{
+      opacity:1,
+      y:0,
+      delay:0.5,
+      stagger:0.1
+    })
+    gsap.fromTo('.num',{
+      opacity:0,
+      y:20
+    },{
+      opacity:1,
+      y:0,
+      delay:0.9
+    })
+    gsap.fromTo('.des',{
+      opacity:0,
+      y:20
+    },{
+      opacity:1,
+      y:0,
+      delay:1
+    })
+  },[])
   return (
     <section
       id="Hero"
@@ -8,19 +42,19 @@ const Hero = () => {
       style={{ backgroundImage: 'url("imgs/coffee-beans-1(1).png")' }}
     >
       <div className="flex flex-col items-center max-w-162 max-md:max-w-125">
-        <h1 className="text-red-600 font-dancing font-bold text-7xl mb-2 max-md:text-5xl max-md:mb-0">
+        <h1 className="text-red-600 font-dancing font-bold text-7xl mb-2 max-md:text-5xl max-md:mb-0 opacity-0 translate-y-10 hero-h1">
           A sip of joy
         </h1>
-        <p className="text-2xl text-white mb-4 max-md:text-[19px] max-md:mb-0">
+        <p className="text-2xl text-white max-md:text-[19px] max-md:mb-0 opacity-0 translate-y-10 hero-p">
           A unique world of flavers
         </p>
-        <p className="text-white text-[1rem] text-center mb-6">
+        <p className="text-white text-[1rem] text-center mb-6 hero-p">
           Discover freshly roasted coffee crafted with passion, sourced from the
           finest farms around the world. Whether you crave a bold espresso or a
           smooth, creamy latte, every cup is brewed to awaken your senses and
           brighten your day.
         </p>
-        <div className="flex gap-4 justify-center items-center mt-1 mb-7  max-md:mb-2">
+        <div className="flex gap-4 justify-center items-center mt-1 mb-7 max-md:mb-2 hero-p">
           <HashLink
             to="#Menu"
             className="text-white bg-red-700 text-[1rem] px-5 py-2 rounded-md shadow-md hover:bg-red-800"
@@ -36,16 +70,16 @@ const Hero = () => {
         </div>
         <div className="flex gap-8 text-green-700 px-6 py-4">
           <div className="text-center">
-            <h2 className="text-2xl font-bold text-brown-800">50k+</h2>
-            <p className="text-sm text-gray-400">Happy Customers</p>
+            <h2 className="text-2xl font-bold text-brown-800 num">50k+</h2>
+            <p className="text-sm text-gray-400 des">Happy Customers</p>
           </div>
           <div className="text-center">
-            <h2 className="text-2xl font-bold text-brown-800">100%</h2>
-            <p className="text-sm text-gray-400">Organic Beans</p>
+            <h2 className="text-2xl font-bold text-brown-800 num">100%</h2>
+            <p className="text-sm text-gray-400 des">Organic Beans</p>
           </div>
           <div className="text-center">
-            <h2 className="text-2xl font-bold text-brown-800">10+</h2>
-            <p className="text-sm text-gray-400">Years of Excellence</p>
+            <h2 className="text-2xl font-bold text-brown-800 num">10+</h2>
+            <p className="text-sm text-gray-400 des">Years of Excellence</p>
           </div>
         </div>
       </div>
